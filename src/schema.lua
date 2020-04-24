0b053714d61f901fddf6b7819c7eac2685b06add
Guilherme Francisquini <guilherme.francisquini@hotmail.com>
2020-04-24 16:38:31 -0300

    Add field path_prefix
    Add field path_prefix

--- src/schema.lua
+++ src/schema.lua
@@ -2,6 +2,7 @@ return {
   no_consumer = true,
   fields = {
     url = { required = true, type = "url" },
+    path_prefix = { default = "", type = "string" },
     connect_timeout = { default = 10000, type = "number" },
     send_timeout = { default = 60000, type = "number" },
     read_timeout = { default = 60000, type = "number" }
